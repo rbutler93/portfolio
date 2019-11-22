01bfc2f7341a36dc702fb2f485a1fad3bdfea221
Rashad Butler <rashadqbutler@gmail.com>
2019-11-21 20:25:51 -0800

    portfolio commit

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+  
+$(".move").on("click", function( e ) {
+    
+    e.preventDefault();
+
+    $("body, html").animate({ 
+        scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+    
+});
